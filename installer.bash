64756a584c934c5b43264d07dafcb3705402afd3
Konstantin <costa.mail@ya.ru>
2017-06-02 13:47:09 +0300

    Updating installing

--- installer.bash
+++ installer.bash
@@ -5,13 +5,13 @@
 
 curl -o ~/.bash_helper https://raw.githubusercontent.com/CostaRico/bash-helper/master/.bashrc
 
-if grep -q bash_helper "~/.bash_helper"; then
-
-   else
+if grep -q bash_helper ~/.bashrc;
+then
+        echo "Updating helper..";
+else
+        echo "Installing helper..";
      echo "
-     source ~/.bash_helper" >> ~/.bashrc 
-fi
-
-
+        source ~/.bash_helper" >> ~/.bashrc;
+fi;
 
 exec bash
